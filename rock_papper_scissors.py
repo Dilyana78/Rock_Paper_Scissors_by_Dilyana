f53e6918df2f0a2a5eb9dabedae94ce107ba1cf0
Dilyana78 <122824954+Dilyana78@users.noreply.github.com>
2023-01-22 14:56:33 +0200

    Add something new to the code
    Added while loop in order the code execute several times while player become winner or lose the game!

--- rock_papper_scissors.py
+++ rock_papper_scissors.py
@@ -2,31 +2,42 @@ import random
 rock = "Rock"
 paper = "Paper"
 scissors = "Scissors"
-player_move = input("Choose [r]ock, [p]aper, [s]cissors: ")
 
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
-else:
-    exit("Invalid input. Try again!")
+win_counter = 0
+lose_counter = 0
+while True:
+    player_move = input("Choose [r]ock, [p]aper, [s]cissors: ")
+    if player_move == "r":
+        player_move = rock
+    elif player_move == "p":
+        player_move = paper
+    elif player_move == "s":
+        player_move = scissors
+    else:
+        exit("Invalid input. Try again!")
 
-computer_random_number = random.randint(1, 3)
-computer_move = ""
-if computer_random_number == 1:
-    computer_move = rock
-elif computer_random_number == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
-print(f"The computer chose {computer_move}.")
+    computer_random_number = random.randint(1, 3)
+    computer_move = ""
+    if computer_random_number == 1:
+        computer_move = rock
+    elif computer_random_number == 2:
+        computer_move = paper
+    else:
+        computer_move = scissors
+    print(f"The computer chose {computer_move}.")
 
-if (player_move == rock and computer_move == scissors) or (player_move == scissors and computer_move == paper) \
-    or (player_move == paper and computer_move == rock):
-    print("You win!")
-elif player_move == computer_move:
-    print("Draw")
-else:
-    print("You lose!")
+    if (player_move == rock and computer_move == scissors) or (player_move == scissors and computer_move == paper) \
+        or (player_move == paper and computer_move == rock):
+        print("You win!")
+        win_counter += 1
+    elif player_move == computer_move:
+        print("Draw")
+    else:
+        print("You lose!")
+        lose_counter += 1
+    if (win_counter == 2 and lose_counter == 0) or (win_counter == 2 and lose_counter == 1):
+        print("You are the winner!")
+        break
+    elif lose_counter == 2:
+        print("You lost the game!")
+        break
